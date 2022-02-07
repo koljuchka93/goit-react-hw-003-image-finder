39cf35e7cd76086a3c57e7ac9ac4f7d9239f4196
Kateryna <koljuchka93@gmail.com>
2022-02-07 20:43:05 +0200

    add commit

--- src/components/Loader/Loader.js
+++ src/components/Loader/Loader.js
@@ -1,10 +1,10 @@
-import Template from 'react-loader-spinner';
+import {TailSpin} from 'react-loader-spinner';
 
 import styles from './loader.module.scss';
 
 const Loader = () => (
   <div className={styles.Loader}>
-    <Template type="TailSpin" color="#02be6e" height={100} width={100} />
+    <TailSpin  color="#02be6e" height={100} width={100} />
   </div>
 );
 
